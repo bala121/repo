f41902dc4098e4dc3651ee8e30c7200028e98d9c
bala121 <fterribas@hotmail.com>
2018-05-21 15:29:04 +0200

    cercle constructors

---
+++ Cercle.java
@@ -0,0 +1,57 @@
+public class Cercle {
+	private float x;
+	private float y;
+        
+	
+        
+        public Cercle(){
+        this.x=6;
+        this.y=7;
+        
+        }
+        
+    //constructora per parametres
+        public Cercle (float xx, float yy)
+        {
+            this.x=xx;
+            this.y=yy;
+        }
+    
+    
+    //constructora copiadora
+        public Cercle(Cercle c){
+            this.x=c.x;
+            this.y=c.y;
+        }
+    
+
+    @Override
+    public String toString() {
+        return "Cercle{" + "x=" + x + ", y=" + y + '}';
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cercle other = (Cercle) obj;
+        if (Float.floatToIntBits(this.x) != Float.floatToIntBits(other.x)) {
+            return false;
+        }
+        if (Float.floatToIntBits(this.y) != Float.floatToIntBits(other.y)) {
+            return false;
+        }
+        return true;
+    }
+
+
+
+}
