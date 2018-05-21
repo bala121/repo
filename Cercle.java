dbb4fcfa9a380b298cb43ebb4a7bcc90a2b4ef28
bala121 <fterribas@hotmail.com>
2018-05-21 15:34:10 +0200

    cercle funcions

--- Cercle.java
+++ Cercle.java
@@ -52,6 +52,24 @@ public class Cercle {
         return true;
     }
 
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+    
+    
+
 
 
 }
